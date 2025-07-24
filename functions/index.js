6a8fdabc8b6dd33065b142f5fe22e7c80d322e4b
AMCS/reyesja <jose.a.reyes@dematic.com>
2025-07-24 05:35:34 -0600

    other test

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions");
 
 exports.verifyAdmin = functions.https.onCall((data, context) => {
-  functions.logger.log("Password test function was called with data:", data);
-  const receivedPassword = data.password || "[No password was received]";
-  // This function always returns a unique success message.
-  return {result: `Hello World, the server received the password: '${receivedPassword}'` };
+  // We expect 'data' to be the password string itself.
+  functions.logger.log("Direct data test. Received:", data);
 
+  const receivedData = data || "[No data was received]";
+
+  return {result: `The server received the direct data: '${receivedData}'`};
 });
