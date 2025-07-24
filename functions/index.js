f95d80b2e3d173d4f2f8b5632c0603062007ed7f
AMCS/reyesja <jose.a.reyes@dematic.com>
2025-07-24 04:59:02 -0600

    Testing Making it safer again (Echo Test)

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,13 @@
 // functions/index.js
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
 
-admin.initializeApp();
+exports.verifyAdmin = functions.https.onCall((data, context) => {
+  // This function will log whatever data it receives...
+  functions.logger.log("Echo function received data:", data);
 
-exports.verifyAdmin = functions.https.onCall(async (data, context) => {
-  const submittedPassword = data.password;
-
-  if (!submittedPassword) {
-    throw new functions.https.HttpsError(
-        "invalid-argument",
-        "The function must be called with a 'password' argument.",
-    );
-  }
-
-  try {
-    const db = admin.database();
-    const snapshot = await db.ref("admin/password").once("value");
-    const actualPassword = snapshot.val();
-
-    if (submittedPassword === actualPassword) {
-      return {success: true};
-    } else {
-      return {success: false};
-    }
-  } catch (error) {
-    console.error("Error verifying admin password:", error);
-    throw new functions.https.HttpsError(
-        "internal",
-        "An internal error occurred while verifying the password.",
-    );
-  }
+  // ...and then send it straight back to the browser.
+  return {
+    iReceivedThis: data,
+    message: "Data was echoed successfully by the server.",
+  };
 });
