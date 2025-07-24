3a15b70a0c7b198468a49c32804ec9d785cd2e9f
AMCS/reyesja <jose.a.reyes@dematic.com>
2025-07-24 05:17:33 -0600

    did not work, one more test!

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,7 @@
-// functions/index.js
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
 
-admin.initializeApp();
-
-exports.verifyAdmin = functions.https.onCall(async (data, context) => {
-  const submittedPassword = data.password;
-
-  if (!submittedPassword) {
-    throw new functions.https.HttpsError(
-        "invalid-argument",
-        "The function must be called with a 'password' argument.",
-    );
-  }
-
-  try {
-    const db = admin.database();
-    const snapshot = await db.ref("admin/password").once("value");
-    const actualPassword = snapshot.val();
-
-    if (submittedPassword === actualPassword) {
-      return {success: true};
-    } else {
-      return {success: false};
-    }
-  } catch (error) {
-    functions.logger.error("Error verifying admin password:", error);
-    throw new functions.https.HttpsError(
-        "internal",
-        "An internal error occurred while verifying the password.",
-    );
-  }
+exports.verifyAdmin = functions.https.onCall((data, context) => {
+  functions.logger.log("Hello World function was called!");
+  // This function always returns a unique success message.
+  return {result: "The new Hello World function is working!"};
 });
